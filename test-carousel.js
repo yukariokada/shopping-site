c5be8e6d8f0079c425b614bcffea202983fb023d
EC2 Default User <ec2-user@ip-172-31-92-123.ec2.internal>
2018-12-26 06:28:40 +0000

    shopping-site

--- test-carousel.js
+++ test-carousel.js
@@ -16,3 +16,11 @@ $(document).ready(function(){
     $(".sp_menu_toggle").slideToggle();
   });
 });
+
+
+$(window).load(function(){
+  $("#list_img img").click(function(){
+    var img_src = $(this).attr("src");
+    $("#display_img img").attr("src", img_src);
+  });
+});
